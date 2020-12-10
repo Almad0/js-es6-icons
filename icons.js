d0a146e3b54bfff32ec0c7aa1cc6f4e57f3aa266
Alessandro <internicola.alessandro@gmail.com>
2020-12-10 16:14:31 +0100

    inserito icone nel div
    Definita una class contructor per le icone, detinito un array di oggetti, selezionato il div e stampato tramife forEach e append su documento html

--- icons.js
+++ icons.js
@@ -1,31 +1,66 @@
-$(document).ready(function () {
+$(function () {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family
-
 
   class icon {
     constructor(name, prefix, type, family) {
       this.name = name;
       this.prefix = prefix;
       this.type = type;
-      this.family = family
+      this.family = family;
     }
   }
 
+  const iconsArray = [
+    new icon("cat", "fas", "fa-cat", "animals"),
+    new icon("crow", "fas", "fa-crow", "animals"),
+    new icon("dog", "fas", "fa-dog", "animals"),
+    new icon("dove", "fas", "fa-dove", "animals"),
+    new icon("dragon", "fas", "fa-dragon", "animals"),
+    new icon("horse", "fas", "fa-horse", "animals"),
+    new icon("hippo", "fas", "fa-hippo", "animals"),
+    new icon("fish", "fas", "fa-fish", "animals"),
+    new icon("carrot", "fas", "fa-carrot", "vegetables"),
+    new icon("lemon", "fas", "fa-lemon", "vegetables"),
+    new icon("apple-alt", "fas", "fa-apple-alt", "vegetables"),
+    new icon("chess-king", "fas", "fa-chess-king", "chess-pieces"),
+    new icon("chess-queen", "fas", "fa-chess-queen", "chess-pieces"),
+    new icon("chess-knight", "fas", "fa-chess-knight", "chess-pieces"),
+    new icon("chess-bishop", "fas", "fa-chess-bishop", "chess-pieces"),
+    new icon("chess-rook", "fas", "fa-chess-rook", "chess-pieces")
+  ]
 
-
-const iconsArray = [
-  {
-    name:cat,
-    prefix:fas,
-    type: fa-cat
-    family: animals
-  }
-]
+  console.log(iconsArray);
   // Semezioniamo il container icons
+
+  let container = $(".icons");
+
+  let print = iconsArray.forEach((icon) => {
+    container.append(`
+      <i class="${icon.prefix} ${icon.type}"></i>
+    `);
+  });
+
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /* ---- FUNCTIONS ----*/
